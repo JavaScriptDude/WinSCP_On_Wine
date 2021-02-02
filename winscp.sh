84f2e5535c8d37746b21b152c61ad6f321c8c1e0
JavaScriptDude <tim.c.quinn@gmail.com>
2021-02-02 14:27:21 -0500

    updating

--- winscp.sh
+++ winscp.sh
@@ -1,33 +1,4 @@
 #! /bin/sh
-
-# Store the following files in a local folder like $HOME/apps/winscp:
-# . winscp.sh
-# . WinSCP.desktop
-# . WinSCP-Logo.png
-# . `cd` to this directory in a terminal window for the balance of steps
-# . Make winscp.sh executable:
-# % chmod a+x winscp.sh
-
-# Installation of WinSCP:
-# % export WINEARCH=win64; export WINEPREFIX=$HOME/.wine_winscp
-# % mkdir -p $WINEPREFIX/drive_c/apps/winscp
-# % winecfg
-#    . (no changes needed) Click ok
-# . Download `Portable executables` version of WinSCP from https://winscp.net
-# . CLI Trick to open in default file manager:
-# % xdg-open $WINEPREFIX/drive_c/apps/winscp
-# . Manually copy over files over to $WINEPREFIX/drive_c/apps/winscp
-
-# *nix executable deployment:
-# % sudo ln -s `pwd`/winscp.sh /usr/bin/winscp
-# . try running the following from commandline: 
-#   % winscp &
-#   . This should launch winscp GUI
-
-# Desktop Installation:
-# % cp ./WinSCP-Logo.png $HOME/.local/share/icons
-# % desktop-file-install --dir=$HOME/.local/share/applications ./WinSCP.desktop 
-
 export WINEARCH=win64 
 export WINEPREFIX=${HOME}/.wine_winscp 
 APP_PATH=${WINEPREFIX}/drive_c/apps/winscp
